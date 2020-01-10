85543055aa9684ae30c021aecc784e9488c0aff8
Tabbs <larsona@outlook.com>
2020-01-09 23:51:15 -0600

    Discord.py rewrite + v-test

--- main.py
+++ main.py
@@ -24,20 +24,41 @@ async def on_ready():
 async def ask(
 	context: Context,
 	question: str, 
-	validator = lambda response: True,
-	invalid_string = "Sorry, that's not a valid response. "
+	validator = lambda message : True, # f(str) -> bool """Returns True if this message is a valid response"""
+	author_only = True,
 	) -> str:
 	
-	user = await context.author()
+	# user = await context.author()
+	def check(author):
+		def trueCheck(message):
+			return (message.author == author or not author_only) and validator(str(message.content))
+		return trueCheck
+	ping: str = ""
+	if context.channel.type == discord.ChannelType.private:
+		ping += "<@{}> ".format(context.author.id)
+	await context.send((ping if ping else "") + question)
+	try:
+		return await bot.wait_for("message", check = check(context.author), timeout=5)
+	except:
+		await context.send("Sorry, you've timed out.")
 	
-	
-
-
 @bot.command(aliases = ["mkch", "mkchannel"], brief = "Nope, not yet.")
 async def addChannel(context: Context):
 	await context.send("Sorry, placeholder.")
 	pass;
 
+
+@bot.command(aliases = ["test"])
+async def testAsk(context: Context):
+	def testInt(msg): 
+		try:
+			int(msg)
+			return True
+		except:
+			return False
+	tempInt = await ask(context, "Please enter a number, anything else will be ignored.", validator=testInt)
+	if tempInt:
+		await context.send("You entered: " + tempInt.content)
 
 @bot.command(brief = "hello my friend")
 async def ping(context: Context):
